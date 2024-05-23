076e7973dcf74fd430c85fe211fbc8d31b08f635
Peretta <viniassisperetta@gmail.com>
2024-05-23 14:21:28 -0300

    Solicitação para API externa feita

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,29 @@
 package org.example
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    // cria um novo cliente http
+    val client: HttpClient = HttpClient.newHttpClient()
+
+    // Prepara uma solicitação para API externa com o método GET
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .build()
+
+
+    // envia a solicitaão para a API e retorna a resposta como uma String
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    //  q   rmazena o corpo da respota JSON em uma variável
+    val json = response.body()
+    println(json)
 }
