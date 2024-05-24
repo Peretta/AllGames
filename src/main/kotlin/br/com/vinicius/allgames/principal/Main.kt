b72f244215e26a725b2b0b38e48dc765306a76cd
Peretta <viniassisperetta@gmail.com>
2024-05-24 14:24:57 -0300

    Iteração realizada na função main

--- src/main/kotlin/br/com/vinicius/allgames/principal/Main.kt
+++ src/main/kotlin/br/com/vinicius/allgames/principal/Main.kt
@@ -13,51 +13,58 @@ fun main() {
 
     // Leitura permite ao Sistema receber informações do teclado do usuário
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar: ")
 
-    // variável 'busca' recebe a informação do usuário
-    val busca = leitura.nextLine()
+    do{
+        println("Digite um código de jogo para buscar: ")
 
-    // a variável meu jogo é criada, é da classe Jogo, inicialmente vazia
-    var meuJogo: Jogo? = null
+        // variável 'busca' recebe a informação do usuário
+        val busca = leitura.nextLine()
 
-    // Instancia o objeto que consome a API
-    val buscaApi = ConsumoApi()
+        // a variável meu jogo é criada, é da classe Jogo, inicialmente vazia
+        var meuJogo: Jogo? = null
 
-    // Tratamento de exceção para a busca na API
-    val resultado = runCatching {
+        // Instancia o objeto que consome a API
+        val buscaApi = ConsumoApi()
 
-        // 'informacaoJogo' recebe os dados da API usando o código de busca fornecido pelo usuário
-        val informacaoJogo = buscaApi.buscaJogo(busca)
+        // Tratamento de exceção para a busca na API
+        val resultado = runCatching {
 
-        // Instancia um novo Jogo utilizando os dados obtidos do objeto InfoJogo
+            // 'informacaoJogo' recebe os dados da API usando o código de busca fornecido pelo usuário
+            val informacaoJogo = buscaApi.buscaJogo(busca)
+
+            // Instancia um novo Jogo utilizando os dados obtidos do objeto InfoJogo
             meuJogo = Jogo(
                 informacaoJogo.info.title,
                 informacaoJogo.info.thumb
-        )
-    }
-    // caso ocorra algum erro na formação do objeto
-    resultado.onFailure {
-        println("Jogo Inexistente. Tente outro id.")
-    }
-    // se o objeto for criado com sucesso
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada? S/N")
-        val opcao = leitura.nextLine()
-        // se a opção for s , o ignore case permite que a verificação não seja sensível a maiúsculas/minúsculas
-        if (opcao.equals("s", true)){
-
-            println("Insira a descrição personalizada para o Jogo: ")
-            val descricao_personalizada = leitura.nextLine()
-            meuJogo?.descricao = descricao_personalizada
-        }else{
-            meuJogo?.descricao = meuJogo?.titulo
+            )
         }
-        // Imprime os detalhes do jogo
-        print(meuJogo)
-    }
+        // caso ocorra algum erro na formação do objeto
+        resultado.onFailure {
+            println("Jogo Inexistente. Tente outro id.")
+        }
+        // se o objeto for criado com sucesso
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada? S/N")
+            val opcao = leitura.nextLine()
+            // se a opção for s , o ignore case permite que a verificação não seja sensível a maiúsculas/minúsculas
+            if (opcao.equals("s", true)){
+
+                println("Insira a descrição personalizada para o Jogo: ")
+                val descricao_personalizada = leitura.nextLine()
+                meuJogo?.descricao = descricao_personalizada
+            }else{
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+            // Imprime os detalhes do jogo
+            println(meuJogo)
+        }
+
+        println("Deseja buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+
+    } while (resposta.equals("s", true))
+
     // Mensagem de sucesso após a busca
-    resultado.onSuccess {
-        println("Busca realizada com sucesso")
-    }
+    println("Busca realizada com sucesso")
+
 }
